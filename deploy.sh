089888af11a1dee8d05ca168243b71a977226599
rajasyed <rajasyedab@gmail.com>
2016-02-22 10:55:17 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 scp -r /var/lib/jenkins/jobs/php_test_deploy/workspace/* mohitbhagwani.s@192.168.4.78:/home/local/PAYODA/mohitbhagwani.s/raja
-cd ~/raja
+cd ~/raja/my-django-blog
 sudo apt-get install python-virtualenv
 virtualenv myvenv
 source myvenv/bin/start
