12b8f9c4d062faf09f5d629ec27ac2bf33a3de05
rajasyed <rajasyedab@gmail.com>
2016-02-22 10:51:49 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
 scp -r /var/lib/jenkins/jobs/php_test_deploy/workspace/* mohitbhagwani.s@192.168.4.78:/home/local/PAYODA/mohitbhagwani.s/raja
 cd ~/raja
-python manage.py blog
+sudo apt-get install python-virtualenv
+virtualenv myvenv
+source myvenv/bin/start
+pip install django==1.9
+django-admin startproject mysite .
+python manage.py makemigrations
+python manage.py migrate
+python manage.py runsever 0.0.0.0:8989
