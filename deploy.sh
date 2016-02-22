660b21c1987d7e767a7ecf75213835f870e17534
rajasyed <rajasyedab@gmail.com>
2016-02-22 11:15:43 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 scp -r /var/lib/jenkins/jobs/php_test_deploy/workspace/* mohitbhagwani.s@192.168.4.78:/home/local/PAYODA/mohitbhagwani.s/raja
 ssh mohitbhagwani.s@192.168.4.78
-cd /home/local/PAYODA/mohitbhagwani.s/raja/my-django-blog
-sudo apt-get install python-virtualenv
-virtualenv myvenv_Jenkin
-source myvenv_Jenkin/bin/start
-pip install django==1.9
-django-admin startproject mysite .
 ls
-python manage.py makemigrations
-python manage.py migrate
-python manage.py runsever 0.0.0.0:8989
+#cd /home/local/PAYODA/mohitbhagwani.s/raja/my-django-blog
+#sudo apt-get install python-virtualenv
+#virtualenv myvenv_Jenkin
+#source myvenv_Jenkin/bin/start
+#pip install django==1.9
+#django-admin startproject mysite .
+#ls
+#python manage.py makemigrations
+#python manage.py migrate
+#python manage.py runsever 0.0.0.0:8989
